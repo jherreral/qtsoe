a44672d8d17a586569abe918deaf855d5cbb76b7
Jaime Herrera <jaimeh@jhnetbook.jhdevices>
2019-04-16 12:59:28 -0400

    Track almost

--- src/main.py
+++ src/main.py
@@ -18,11 +18,23 @@ class Track(QWidget):
         self.setMinimumSize(width,height)
 
         firstCol = QGridLayout()
-        firstCol.addWidget(QLabel("A"),1,1)
-        firstCol.addWidget(QLabel("A"),2,1)
-        firstCol.addWidget(QLabel("A"),3,1)
-        firstCol.addWidget(QLabel("A"),4,1)
+        firstCol.addWidget(QLabel("Prod"),1,1)
+        firstCol.addWidget(QLabel("Oil"),2,1)
+        firstCol.addWidget(QLabel("Sph"),3,1)
+        firstCol.addWidget(QLabel("Caps"),4,1)
         self.mainLayout.addLayout(firstCol,1,1)
+        t_data = [[10,20,30,40],[11,22,33,44],[12,22,32,42],[13,23,33,43]]
+        self.CreatePlayerColumns(t_data)
+
+    def CreatePlayerColumns(self,data):
+        playerColumns = QGridLayout()
+        for idx,p in enumerate(data):
+            col = idx +1
+            playerColumns.addWidget(QLabel(str(p[0])),1,col)
+            playerColumns.addWidget(QLabel(str(p[1])),2,col)
+            playerColumns.addWidget(QLabel(str(p[2])),3,col)
+            playerColumns.addWidget(QLabel(str(p[3])),4,col)
+        self.mainLayout.addLayout(playerColumns,1,2)
 
 
 app = QApplication([])
