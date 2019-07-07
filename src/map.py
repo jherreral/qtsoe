ded6c220aa75744cc622f0f740e2e4b4e4d0bed1
Jaime Herrera <jaimeh@jhnetbook.jhdevices>
2019-07-07 17:57:35 -0400

    Polished OneClickRequest

--- src/map.py
+++ src/map.py
@@ -1,16 +1,20 @@
 from PySide2.QtWidgets import (
     QWidget
 )
-from PySide2.QtCore import Slot
+from PySide2.QtCore import Slot, Signal
 
 from zone import Zone
-from request import ClickRequest
+from request import (
+    OneClickRequest,
+    MultiClickRequest
+)
 
 
 class Map(QWidget):
     """
     Holds all interactive zones
     """
+    requestDone = Signal(str)
 
     def __init__(self, parent=None, width=400, height=450):
         super(Map, self).__init__(parent)
@@ -18,12 +22,13 @@ class Map(QWidget):
         self.zones = {}
         self.parent = parent
         self.request = None
+        self.requestDone.connect(self.parent.log.addEntry)
 
         self.loadFromAssets()
         self.setAllZones(False)
 
         z = ['Chile', 'Argen', 'Ingla']
-        self.setRequest(ClickRequest(z, 6))
+        self.setRequest(OneClickRequest(z, 6))
 
 # Requests...
 # Map should set the state of the zones it is interested in, and receive
@@ -31,21 +36,14 @@ class Map(QWidget):
 # So each request type is defined by its criteria/goal, the possible zones
 # and the current progress.
 #
-# For example, a request like 'CLick 5 countries in South America' could be:
-# Request
-# self.goal = 5
-# self.current = 0
-# self.zonesList = ['Chile','Argen',...]
-# for zone in self.zonesList:
-#     zone.selectable = True
-#     zone.hold = False
-#
-# In Map add updateCurrentRequest() as a slot. Connect all zone.ZoneActivated
-# signals to that slot, so that it checks and updates the conditions.
-#
+
     def setRequest(self, r):
         self.request = r
-        self.setZones(r.availableZones, True)
+        if type(r) == OneClickRequest:
+            self.setZones(r.availableZones, False)
+        elif type(r) == MultiClickRequest:  # Maybe not necessary. Is it used?
+            self.setZones(r.availableZones, True)
+
         for zoneName in r.availableZones:
             self.zones[zoneName].zoneSelected.connect(
                 self.updateCurrentRequest)
@@ -58,6 +56,7 @@ class Map(QWidget):
         if result:
             # Send signal to GM or PlayerView.
             print('Request done')
+            self.requestDone.emit('Request Done: '+self.request.doneMessage())
             self.setAllZones(False)
             self.request = None
 
@@ -85,9 +84,10 @@ class Map(QWidget):
     def createBackground(self):
         pass
 
-    def setZones(self, zoneList, value):
+    def setZones(self, zoneList, hold=False, selectable=True):
         for zoneName in zoneList:
-            self.zones[zoneName].selectable = value
+            self.zones[zoneName].selectable = selectable
+            self.zones[zoneName].hold = hold
 
     def setAllZones(self, value):
         for zoneName in self.zones:
--- src/request.py
+++ src/request.py
@@ -1,6 +1,6 @@
-# class MapRequest:
-#     def __init__(self,rq):
-#         for zone,state
+"""
+Request classes that interact with Map to get the users input.
+"""
 
 
 class Request:
@@ -8,7 +8,7 @@ class Request:
         pass
 
 
-class ClickRequest(Request):
+class OneClickRequest(Request):
     def __init__(self, zones, n):
         self.goal = n
         self.current = 0
@@ -16,12 +16,35 @@ class ClickRequest(Request):
         self.selectedZones = dict(zip(zones, [0]*len(zones)))
         self.hold = False
 
-    def update(self, ev):
-        self.selectedZones[ev] += 1
+    def update(self, zoneName):
+        self.selectedZones[zoneName] += 1
         self.current += 1
 
     def check(self):
         if self.current == self.goal:
+            return self.selectedZones
+        else:
+            return None
+
+    def doneMessage(self):
+        msg = 'Selected'
+        for k in self.selectedZones.keys():
+            msg += '\n' + k + ' ' + str(self.selectedZones[k])
+        return msg
+
+
+class MultiClickRequest(Request):
+    def __init__(self, zones, n):
+        self.goal = n
+        self.availableZones = zones
+        self.selectedZones = set()
+
+    def update(self, zoneName):
+        if zoneName not in self.selectedZones:
+            self.selectedZones.add(zoneName)
+
+    def check(self):
+        if len(self.selectedZones) == self.goal:
             return self.selectedZones
         else:
             return None
