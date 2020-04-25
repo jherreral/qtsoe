f9e52e64ec1c0d21e0df8b60e68261c12d6dea58
jherreral <33136201+jherreral@users.noreply.github.com>
2020-04-25 18:09:51 -0400

    WIP, ordering everything

--- src/soe/__init__.py
+++ src/soe/__init__.py
@@ -1,1 +1,30 @@
-from Core import *
+import .Client
+import .Server
+
+from PySide2.QtWidgets import (
+    QApplication,
+    QMainWindow
+)
+
+
+def _main():
+    app = QApplication([])
+
+    window = QMainWindow()
+    window.setMinimumSize(800, 600)
+
+    player_one_view = Client.PlayerView(window)
+    game_session = Server.GameSession()
+    # zone1.clicked.connect(log.addEntry)
+    # QObject.connect(zone1,SIGNAL('clicked()'),log.addEntry)
+
+    # window.setCentralWidget(log)
+
+    # mylabel.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+    window.show()
+    # mylabel.adjustSize()
+    app.exec_()
+    print(str(window.size()))
+
+if __name__ == "__main__":
+    main()
